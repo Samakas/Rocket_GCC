f1e7f1ab84a6da0a85ed80d7703c3db74b12cdd6
Samakas <samakash1245@gmail.com>
2026-09-16 10:08:07 +0530

    Spring initialized

---
+++ Rocket/src/test/java/com/example/Rocket/RocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
